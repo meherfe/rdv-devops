f5890b5937d55d95fa0efbed7d45d54bf4b12341
maher <meher.fe08@gmail.com>
2025-02-16 14:50:14 +0100

    sonar-correction1

--- src/main/java/esprit/tn/examenrdv/aspects/LoggingAspect.java
+++ src/main/java/esprit/tn/examenrdv/aspects/LoggingAspect.java
@@ -1,19 +1,20 @@
 package esprit.tn.examenrdv.aspects;
 
-
-
 import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Aspect
 @Component
 public class LoggingAspect {
 
+    private static final Logger logger = LoggerFactory.getLogger(LoggingAspect.class);
+
     // Define a pointcut that matches any method in the service package that starts with 'add'
     @After("execution(* esprit.tn.examenrdv.services.*.add*(..))")
     public void logAfterAddMethods() {
-        System.out.println("méthode exécutée");
+        logger.info("méthode exécutée");
     }
 }
-
