d3de3429f4af1a5906769a864376edc3d33b3a2c
maher <meher.fe08@gmail.com>
2025-02-16 15:14:56 +0100

    last update

--- src/main/java/esprit/tn/examenrdv/ExamenRdvApplication.java
+++ src/main/java/esprit/tn/examenrdv/ExamenRdvApplication.java
@@ -1,12 +1,8 @@
 package esprit.tn.examenrdv;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.boot.test.context.SpringBootTest;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
 @SpringBootApplication
 @EnableScheduling
 public class ExamenRdvApplication {
@@ -14,12 +10,5 @@ public class ExamenRdvApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ExamenRdvApplication.class, args);
 	}
-}
 
-@SpringBootTest
-class ExamenRdvApplicationTests {
-	@Test
-	void contextLoads() {
-		assertTrue(true, "Application context should load correctly");
-	}
 }
