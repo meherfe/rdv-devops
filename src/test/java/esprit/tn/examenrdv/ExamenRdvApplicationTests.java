a2330722436b5cd09d8d5a41824842a180fd9378
maher <meher.fe08@gmail.com>
2025-02-17 16:08:56 +0100

    last update

--- src/test/java/esprit/tn/examenrdv/ExamenRdvApplicationTests.java
+++ src/test/java/esprit/tn/examenrdv/ExamenRdvApplicationTests.java
@@ -1,14 +1,20 @@
 package esprit.tn.examenrdv;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class ExamenRdvApplicationTests {
 
+	@Autowired
+	private ApplicationContext context;
+
 	@Test
 	void contextLoads() {
-		//this is a test feature
+		assertThat(context).isNotNull();
 	}
-
 }
